d606e7e6586a96e0eb8387b3390c086056acf5c1
Chandan Kumar <chandank@skucaster.com>
2023-12-31 11:39:14 -0500

    updated google analytics ID

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import ReactGA from 'react-ga';
 
 const App = () => {
   useEffect(() => {
-    ReactGA.initialize('G-C8Q26L8672'); // Replace with your Google Analytics tracking ID
+    ReactGA.initialize('G-ZTKLPD4Z2X'); // Replace with your Google Analytics tracking ID
     ReactGA.pageview(window.location.pathname + window.location.search);
   }, []);
 
